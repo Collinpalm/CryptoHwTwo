d2cca908357e1681bcd595c44b037fb2927ef9c0
Collin Palm <collinpalm@gmail.com>
2021-09-10 19:26:23 -0400

    more code

--- vigenere.py
+++ vigenere.py
@@ -15,11 +15,15 @@ def modInverse(a, m):
             return x
     return -1
 
-def findSubStrings(cipher, keylen):
+def findKeyLen(cipher, bins, length):
+    for el in range(0, len(cipher)):
+        i=0
+
+def findSubStrings(cipher, keylen, length):
     subStrings = {}
     str = ""
 
-    for x in range(keylen-1, len(cipher)):
+    for x in range(keylen-1, length):
         for element in range(x-keylen, x):
             str += cipher[element]
         if str in subStrings:
@@ -31,17 +35,18 @@ def findSubStrings(cipher, keylen):
 
 def decrypt():
     ciphertext = sys.argv[1]
+    length = len(ciphertext)
     bins = []
     possible = []
     bins[0] = "placeholder"
     bins[1] = findSubStrings(ciphertext, 1)
     bins[1] = sorted(bins[1].items(), key=lambda item:item[1], reverse=True)
     for x in range(2, 14):
-        bins.append({x: findSubStrings(ciphertext, x)})
+        bins.append({x: findSubStrings(ciphertext, x, length)})
         bins[x] = sorted(bins[x].items(), key=lambda item:item[1], reverse=True)
         if list(bins[x].values())[0] > 1:
             possible.append(x)
-       
+
     for j in range(0, len(possible)):
         i = 0
     
